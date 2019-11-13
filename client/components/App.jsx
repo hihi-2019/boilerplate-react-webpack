ddab85c6446c29b18c85df9aca2ef48bee90f367
Student <student@eda-bc-05.localdomain>
2019-11-13 17:00:34 +1300

    Get dogs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import superagent from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+const loadingGif = 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'
+
+class App extends Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      dogs: []
+    }
+
+    this.refreshDogs = this.refreshDogs.bind(this)
+  }
+
+  componentDidMount() {
+    this.refreshDogs()
+  }
+
+  refreshDogs() {
+    this.setState({
+      dogs: []
+    })
+    superagent.get('/api/dogs')
+    .then(res => {
+      this.setState({
+        dogs: res.body
+      })
+    })
+  }
+  
+  render() {
+    return (
+      <>
+      <h1>Dogs!</h1>
+      <div className="buttons">
+        <button onClick={this.refreshDogs}>Get New Dogs</button>
+      </div>
+      <div className="dogs">
+        { this.state.dogs ?
+          Object.keys(this.state.dogs).map(key => {
+            return (<img key={key} src={this.state.dogs[key]}></img>)
+          }) :
+          <img src={loadingGif} />
+        }
+      </div>
+      </>
+    )
+  }
+
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const path = require('path')
 const express = require('express')
+const superagent = require('superagent')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+server.get('/api/dogs', (req, res) => {
+  superagent.get('http://shibe.online/api/shibes?count=10')
+    .set('accept', 'json')
+    .then(data => {
+      res.json(JSON.parse(data.text))
+    })
+})
 
 module.exports = server
