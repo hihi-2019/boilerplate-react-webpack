18d234889fd403afc5353a9bb951f388c1b3161f
Student <student@eda-bc-06.localdomain>
2019-11-13 17:21:09 +1300

    Finished cat facts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      cats: []
+    }
+  }
+
+  componentDidMount(){
+    this.getCats()
+  }
+
+  getCats(){
+    request.get('/cats')
+    .then(cats => {
+      console.log(cats.body.all)
+      this.setState({
+        cats: cats.body.all
+      })
+    })
+  }
+
+
+ 
+
+  render() {
+    return(
+      <>
+      <h1>FUN CAT FACTS AHEAD</h1>
+      {this.state.cats ? (
+        <section>
+          {this.state.cats.map((fact, i) => {
+          return <div><h1>CAT FACT: {i+1} </h1><p>{fact.text}</p><h4>UPVOTES: {fact.upvotes}</h4><br></br><br></br></div>
+        })}
+        </section>
+        
+      ) :  <img src="https://gph.is/1XRTmuh"/>}
+      
+      </>
+    )
+    
+  }
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const path = require('path')
 const express = require('express')
+const request = require('superagent')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+
+server.get('/cats', (req, res) => {
+    request.get("https://cat-fact.herokuapp.com/facts")
+    .then(apiRes => res.send(apiRes.body))
+})
 
 module.exports = server
