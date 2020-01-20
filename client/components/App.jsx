9cced54e7e7b6723ba7c5f54ff3f9027ce2986a3
rlyhan <richard_han@live.com>
2020-01-20 20:55:50 +1300

    Initial file setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import Contact from './Contact'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={ () => <Home /> } />
+        <Route exact path="/about" render={ () => <About /> } />
+        <Route exact path="/projects" render={ () => <Projects /> } />
+        <Route exact path="/contact" render={ () => <Contact /> } />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
