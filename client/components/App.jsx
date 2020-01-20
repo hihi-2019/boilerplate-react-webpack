c40f99402899a012cef2262818b9190c1d5b5141
rlyhan <richard_han@live.com>
2020-01-20 21:05:06 +1300

    Oops

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import Home from './Home'
-import About from './About'
-import Projects from './Projects'
-import Contact from './Contact'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={ () => <Home /> } />
-        <Route exact path="/about" render={ () => <About /> } />
-        <Route exact path="/projects" render={ () => <Projects /> } />
-        <Route exact path="/contact" render={ () => <Contact /> } />
-      </Switch>
-    </BrowserRouter>
+    <h1>React development has begun!</h1>
   )
 }
 
